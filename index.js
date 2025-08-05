96ab20774831d18916beee8ec45e0325a7f9689a
Sibil Arockian G <sibil3092@gmail.com>
2025-08-05 18:05:38 +0530

    second commit

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 
 const PORT = process.env.port || 5000;
+
+//API key Welcome to Alpha Vantage! Your dedicated access key is: OQOCVU5FVU5CFOS8. Please record this API key at a safe place for future data access.
+
+request('https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=OQOCVU5FVU5CFOS8', {json: true}, (err, res, body) => {
+	if(err){return console.log(err);}
+	console.log(body);
+	if(res.statusCode == 200){
+		console.log(body);
+	};
+});
+
+
+
 
 //Set Handlebars Middleware
 
@@ -18,6 +32,11 @@ app.get('/', function (req, res){
     res.render('home', {
     	stuff: "This is stuff..."
     });
+});
+
+//create about page route
+app.get('/about.html', function (req, res){
+    res.render('about');
 });
 
 
